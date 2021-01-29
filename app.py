169eedf16f677e7b984896b9ce57633fe099f76d
tejavoo <teja.3b@gmail.com>
2021-01-29 17:35:38 +0530

    naya_form

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import json
 
 app = Flask(__name__)
 
+# https://hevodata.com/learn/mongoexport-export-mongodb-collections/#:~:text=Mongoexport%20is%20a%20command%2Dline,through%20the%20MongoDB%20Download%20Center.
+
 app.config['MONGO_DBNAME'] = 'restdb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
 
@@ -18,7 +20,7 @@ mongo = PyMongo(app)
 @app.route('/', methods=['GET'])
 def get_all_stars():
 
-    return render_template("index.html")
+    return render_template("index2.html")
 
 @app.route('/favicon.ico') 
 def favicon(): 
@@ -30,7 +32,8 @@ def add_star():
     idatadb = mongo.db.idatadbra
     req_data = request.form
     # print(req_data)
-    idatadb.insert_one({'a':req_data['a'], 'b':req_data['b'], 'c':req_data['c'], 'd':req_data['d'], 'e':req_data['e'], 'f':req_data['f'], 'g':req_data['g'], 'h':req_data['h'], 'i':req_data['i']})
+    idatadb.insert_one({'WhoHelpedYouFindThisForm':req_data['a'], 'Name':req_data['b'], 'Email':req_data['c'], 'Address':req_data['d'], 'PhoneNumber':req_data['e'], 'DefineYourself':req_data['f'], 'Followers':req_data['g'], 'InstaUsername':req_data['h'], 'Average_Likes':req_data['i']})
+    # return ('', 204)
     return render_template("index_hogaya.html")
 #     return "<h1></h1>"
 
--- static/scripts.js
+++ static/scripts.js
@@ -1,154 +1,154 @@
 // https://www.tutorialspoint.com/jquery/effect-scale.htm
 
 
-$(".in1").focus(function() { 
-    $(".im1").css("display", "block");
-    $(".im1").css("transition", "width 1s ease-in-out");
-    $(".im1").css("width", "60%"); 
-    $(".im1").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "pink");
-});
-
-$(".in1").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im1").css("display", "block");
-    $(".im1").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-
-$(".in2").focus(function() { 
-    $(".im2").css("display", "block");
-    $(".im2").css("transition", "width 1s ease-in-out");
-    $(".im2").css("width", "60%"); 
-    $(".im2").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in2").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im2").css("display", "block");
-    $(".im2").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in3").focus(function() { 
-    $(".im3").css("display", "block");
-    $(".im3").css("transition", "width 1s ease-in-out");
-    $(".im3").css("width", "60%"); 
-    $(".im3").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in3").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im3").css("display", "block");
-    $(".im3").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in4").focus(function() { 
-    $(".im4").css("display", "block");
-    $(".im4").css("transition", "width 1s ease-in-out");
-    $(".im4").css("width", "60%"); 
-    $(".im4").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in4").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im4").css("display", "block");
-    $(".im4").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in5").focus(function() { 
-    $(".im5").css("display", "block");
-    $(".im5").css("transition", "width 1s ease-in-out");
-    $(".im5").css("width", "60%"); 
-    $(".im5").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in5").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im5").css("display", "block");
-    $(".im5").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in6").focus(function() { 
-    $(".im6").css("display", "block");
-    $(".im6").css("transition", "width 1s ease-in-out");
-    $(".im6").css("width", "60%"); 
-    $(".im6").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in6").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im6").css("display", "block");
-    $(".im6").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in7").focus(function() { 
-    $(".im7").css("display", "block");
-    $(".im7").css("transition", "width 1s ease-in-out");
-    $(".im7").css("width", "60%"); 
-    $(".im7").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in7").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im7").css("display", "block");
-    $(".im7").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in8").focus(function() { 
-    $(".im8").css("display", "block");
-    $(".im8").css("transition", "width 1s ease-in-out");
-    $(".im8").css("width", "60%"); 
-    $(".im8").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in8").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im8").css("display", "block");
-    $(".im8").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-
-$(".in8").focus(function() { 
-    $(".im8").css("display", "block");
-    $(".im8").css("transition", "width 1s ease-in-out");
-    $(".im8").css("width", "60%"); 
-    $(".im8").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "#a8c753");
-});
-
-$(".in8").focusout(function() { 
-    // $(this).next("span").css("display", "inline"); 
-    $(".im8").css("display", "block");
-    $(".im8").css("width", "60%"); 
-    $(".typeform-area").css("background-color", "aqua");
-});
-// $(".in0").focus(function() { 
-//     $(".im1").css("display", "inline");
+// $(".in1").focus(function() { 
+//     $(".im1").css("display", "block");
 //     $(".im1").css("transition", "width 1s ease-in-out");
-//     $(".im1").css("width", "100%"); 
-//     $(".typeform-area").css("background-color", "pink")
+//     $(".im1").css("width", "60%"); 
+//     $(".im1").css("width", "60%"); 
+//     $("#main").css("background-color", "pink");
 // });
 
-// $(".in0").focusout(function() { 
+// $(".in1").focusout(function() { 
 //     // $(this).next("span").css("display", "inline"); 
-//     $(".im1").css("display", "none");
-//     $(".im1").css("width", "50px"); 
-//     $(".typeform-area").css("background-color", "aqua")
+//     $(".im1").css("display", "block");
+//     $(".im1").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
 // });
+
+
+// $(".in2").focus(function() { 
+//     $(".im2").css("display", "block");
+//     $(".im2").css("transition", "width 1s ease-in-out");
+//     $(".im2").css("width", "60%"); 
+//     $(".im2").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in2").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im2").css("display", "block");
+//     $(".im2").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in3").focus(function() { 
+//     $(".im3").css("display", "block");
+//     $(".im3").css("transition", "width 1s ease-in-out");
+//     $(".im3").css("width", "60%"); 
+//     $(".im3").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in3").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im3").css("display", "block");
+//     $(".im3").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in4").focus(function() { 
+//     $(".im4").css("display", "block");
+//     $(".im4").css("transition", "width 1s ease-in-out");
+//     $(".im4").css("width", "60%"); 
+//     $(".im4").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in4").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im4").css("display", "block");
+//     $(".im4").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in5").focus(function() { 
+//     $(".im5").css("display", "block");
+//     $(".im5").css("transition", "width 1s ease-in-out");
+//     $(".im5").css("width", "60%"); 
+//     $(".im5").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in5").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im5").css("display", "block");
+//     $(".im5").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in6").focus(function() { 
+//     $(".im6").css("display", "block");
+//     $(".im6").css("transition", "width 1s ease-in-out");
+//     $(".im6").css("width", "60%"); 
+//     $(".im6").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in6").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im6").css("display", "block");
+//     $(".im6").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in7").focus(function() { 
+//     $(".im7").css("display", "block");
+//     $(".im7").css("transition", "width 1s ease-in-out");
+//     $(".im7").css("width", "60%"); 
+//     $(".im7").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in7").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im7").css("display", "block");
+//     $(".im7").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in8").focus(function() { 
+//     $(".im8").css("display", "block");
+//     $(".im8").css("transition", "width 1s ease-in-out");
+//     $(".im8").css("width", "60%"); 
+//     $(".im8").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in8").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im8").css("display", "block");
+//     $(".im8").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+
+// $(".in8").focus(function() { 
+//     $(".im8").css("display", "block");
+//     $(".im8").css("transition", "width 1s ease-in-out");
+//     $(".im8").css("width", "60%"); 
+//     $(".im8").css("width", "60%"); 
+//     $("#main").css("background-color", "#a8c753");
+// });
+
+// $(".in8").focusout(function() { 
+//     // $(this).next("span").css("display", "inline"); 
+//     $(".im8").css("display", "block");
+//     $(".im8").css("width", "60%"); 
+//     $("#main").css("background-color", "aqua");
+// });
+// // $(".in0").focus(function() { 
+// //     $(".im1").css("display", "inline");
+// //     $(".im1").css("transition", "width 1s ease-in-out");
+// //     $(".im1").css("width", "100%"); 
+// //     $("#main").css("background-color", "pink")
+// // });
+
+// // $(".in0").focusout(function() { 
+// //     // $(this).next("span").css("display", "inline"); 
+// //     $(".im1").css("display", "none");
+// //     $(".im1").css("width", "50px"); 
+// //     $("#main").css("background-color", "aqua")
+// // });
 
 
 $(window).ready(function() { 
@@ -161,4 +161,149 @@ $(window).ready(function() {
             return false; 
         } 
     }); 
-    }); 
+    });
+
+$(window).ready(function(){
+
+
+// https://www.tutorialspoint.com/jquery/effect-scale.htm
+
+
+$(".in0").focus(function() { 
+    $(".im1").css("display", "block");
+    $(".im1").animate({width:"60%",opacity:'1'},"slow");
+    $(".im1").css("transition", "width 1s ease-in-out");
+    $(".im1").css("width", "60%"); 
+    $(".im1").css("width", "60%"); 
+    // $("#main").css("background-color", "pink");
+});
+
+$(".in0").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im1").css("display", "block");
+    $(".im1").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+
+$(".in1").focus(function() { 
+    $(".im2").css("display", "block");
+    $(".im2").animate({width:"60%",opacity:'1'},"slow");
+    $(".im2").css("width", "60%");
+    $(".im2").css("width", "60%");
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in1").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im2").css("display", "block");
+    $(".im2").css("width", "60%");
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in2").focus(function() { 
+    $(".im3").css("display", "block");
+    $(".im3").animate({width:"60%",opacity:'1'},"slow");
+    $(".im3").css("width", "60%");
+    $(".im3").css("width", "60%");
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in2").focusout(function() {
+    // $(this).next("span").css("display", "inline");
+    $(".im3").css("display", "block");
+    $(".im3").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in3").focus(function() { 
+    $(".im4").css("display", "block");
+    $(".im4").animate({width:"60%",opacity:'1'},"slow");
+    $(".im4").css("width", "60%"); 
+    $(".im4").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in3").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im4").css("display", "block");
+    $(".im4").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in4").focus(function() { 
+    $(".im5").css("display", "block");
+    $(".im5").animate({width:"60%",opacity:'1'},"slow");
+    $(".im5").css("width", "60%"); 
+    $(".im5").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in4").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im5").css("display", "block");
+    $(".im5").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in5").focus(function() { 
+    $(".im6").css("display", "block");
+    $(".im6").animate({width:"60%",opacity:'1'},"slow");
+    $(".im6").css("width", "60%"); 
+    $(".im6").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in5").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im6").css("display", "block");
+    $(".im6").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in6").focus(function() { 
+    $(".im7").css("display", "block");
+    $(".im7").animate({width:"60%",opacity:'1'},"slow");
+    $(".im7").css("width", "60%"); 
+    $(".im7").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in6").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im7").css("display", "block");
+    $(".im7").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in7").focus(function() { 
+    $(".im8").css("display", "block");
+    $(".im8").animate({width:"60%",opacity:'1'},"slow");
+    $(".im8").css("width", "60%"); 
+    $(".im8").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in7").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im8").css("display", "block");
+    $(".im8").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+$(".in8").focus(function() { 
+    $(".im9").css("display", "block");
+    $(".im9").animate({width:"60%",opacity:'1'},"slow");
+    $(".im9").css("width", "60%"); 
+    $(".im9").css("width", "60%"); 
+    // $("#main").css("background-color", "#a8c753");
+});
+
+$(".in8").focusout(function() { 
+    // $(this).next("span").css("display", "inline"); 
+    $(".im9").css("display", "block");
+    $(".im9").css("width", "60%"); 
+    // $("#main").css("background-color", "aqua");
+});
+
+});
